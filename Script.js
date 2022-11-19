b4646081a41d9ee36cf79c18782439fa26258439
Noreen Malik <113777571+Noreen777@users.noreply.github.com>
2022-11-19 12:40:30 +0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,64 @@
+// using cmd
+// _______________1_______________
+
+// for (var i = 200; i >= 0; i -= 3){
+//       console.log(i);
+//  }
+
+
+// _______________2_______________
+
+//   n = 10;
+//   i= 1;
+// while (i <= n) {
+//     console.log("Noreen")
+//     i++;
+// }
+
+
+// _______________3_______________
+
+
+// let even = 1;
+// let num = 10;
+// let sum = 0;
+// while(even <= num){
+//     if(even % 2 == 0){
+//         sum = sum + even;
+//     }
+//     even++;
+// }
+// console.log("The sum of even numbers from 1 - 10 is:", sum );
+
+
+// _______________4_______________
+
+
+// let number = 1;
+// do {
+//    console.log(number);
+//    number = number + 1;
+// } while (number <=5)
+
+
+// _______________5_______________
+ 
+
+
+//   function add(a, b) {
+//     console.log("The sum of two numbers is ", a + b);
+// }
+
+// add(2, 9);
+
+// _______________6_______________
+
+
+
+// function areaOfrec(width, height) {
+
+//     var area = (width * height);
+//     console.log("The Area of a Rectangle is:", area);
+// }
+
+// var avg = areaOfrec(30, 10);
